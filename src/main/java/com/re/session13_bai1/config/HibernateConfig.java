6f28f575893d78da4f2e3cf1de32c80f9ac22540
Phạm Tiến Hưng <hungpham772006@gmail.com>
2026-04-22 22:39:39 +0700

    commit

--- src/main/java/com/re/session13_bai1/config/HibernateConfig.java
+++ src/main/java/com/re/session13_bai1/config/HibernateConfig.java
@@ -26,6 +26,7 @@ public class HibernateConfig {
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         sessionFactoryBean.setHibernateProperties(properties);
 
         return sessionFactoryBean;
